1ead8135e00c7d5c16ff804e6413e486f77e736c
Alexandre PAGE <alexandre.page@epitech.eu>
2015-05-04 19:19:35 +0200

    map begin

--- inc/Map.hpp
+++ inc/Map.hpp
@@ -9,8 +9,13 @@
 #define	MAP_HPP
 
 #include <string>
+#include <iostream>
 #include "IEntity.hpp"
 #include "Observer.hpp"
+
+#define EMPTY 0;
+#define	SOLID 1;
+#define DESTR 2;
 
 class Map : public IEntity, public Observer, public Subject
 {
@@ -21,7 +26,16 @@ public:
 
 	virtual void onNotify(IEntity const & entity, Event event);
 
+protected:
+	void	generatemap();
+	void 	displaymap();
+	int 	**getmap();
+
 private:
+	void 	delimitMap();
+	int **_map;
+	int _height;
+	int _width;
 
 };
 
--- src/Map.cpp
+++ src/Map.cpp
@@ -7,12 +7,67 @@
 
 #include "Map.hpp"
 
-Map::Map()
+int main()
 {
+	Map m(20, 20);
+
+}
+
+Map::Map(size_t width, size_t height): _width(width), _height(height)
+{
+	std::cout << "Loading..." << std::endl;
+	this->generatemap();
+	this->displaymap();
+}
+
+Map::Map(std::string const & mapFile)
+{
+	this->generatemap();
 }
 
 Map::~Map()
 {
+
+}
+
+void	Map::generatemap()
+{
+	int i;
+	int j;
+
+	i = 0;
+	this->_map = new int *[_height];
+	for (i = 0; i < this->_width; i++)
+		this->_map[i] = new int[this->_width];
+	for (i = 0; i < this->_height; i++)
+	{
+		for (j = 0; j < this->_width; j++)
+			this->_map[i][j] = 0;
+	}
+	this->delimitMap();
+}
+
+void Map::delimitMap()
+{
+	int i;
+
+	for (i = 0; i < this->_width; i++)
+		this->_map[0][i] = SOLID;
+}
+
+void Map::displaymap()
+{	
+	for (int i = 0; i < this->_height; i++)
+	{
+		for (int j = 0; j < this->_width; j++)
+			std::cout << this->_map[i][j];
+		std::cout << std::endl;
+	}
+}
+
+int  **Map::getmap()
+{
+	return (this->_map);
 }
 
 void Map::onNotify(const IEntity& entity, Event event)
