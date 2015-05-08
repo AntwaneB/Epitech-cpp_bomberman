42bb8bf58fc158ec8b3407819da0e1d8b46bce88
François Kiene <francois.kiene@epitech.eu>
2015-05-08 16:56:26 +0200

    Add Graphics

--- src/Graphics/Cube.cpp
+++ src/Graphics/Cube.cpp
@@ -18,101 +18,86 @@ Graphics::Cube::~Cube()
 
 bool Graphics::Cube::initialize()
 {
-std::string path;
-_speed = 10.0f;
-// On charge la texture qui sera affichee sur chaque face du cube
-if (_type == 0)
-	path = "./libgdl/assets/sand.tga";
-else if (_type == 1)
-	path = "./libgdl/assets/sol.tga";
-else
-	path = "./libgdl/assets/wood.tga";
-if(_texture.load(path) == false)
-{
-	std::cout << "false texture" << std::endl;
-	return (false);
-}
-// on set la color d'une premiere face
-//_geometry.setColor(glm::vec4(1, 0, 0, 1));
-// tout les pushVertex qui suivent seront de cette couleur
-// On y push les vertices d une premiere face
-_geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
-_geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
-_geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
-_geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
-// Les UVs d'une premiere face
-_geometry.pushUv(glm::vec2(0.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 1.0f));
-_geometry.pushUv(glm::vec2(0.0f, 1.0f));
-// ETC ETC
-//_geometry.setColor(glm::vec4(1, 1, 0, 1));
-_geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
-_geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
-_geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
-_geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
-_geometry.pushUv(glm::vec2(0.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 1.0f));
-_geometry.pushUv(glm::vec2(0.0f, 1.0f));
-//_geometry.setColor(glm::vec4(0, 1, 1, 1));
-_geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
-_geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
-_geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
-_geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
-_geometry.pushUv(glm::vec2(0.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 1.0f));
-_geometry.pushUv(glm::vec2(0.0f, 1.0f));
-//_geometry.setColor(glm::vec4(1, 0, 1, 1));
-_geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
-_geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
-_geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
-_geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
-_geometry.pushUv(glm::vec2(0.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 1.0f));
-_geometry.pushUv(glm::vec2(0.0f, 1.0f));
-//_geometry.setColor(glm::vec4(0, 1, 0, 1));
-_geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
-_geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
-_geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
-_geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
-_geometry.pushUv(glm::vec2(0.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 1.0f));
-_geometry.pushUv(glm::vec2(0.0f, 1.0f));
-//_geometry.setColor(glm::vec4(0, 0, 1, 1));
-_geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
-_geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
-_geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
-_geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
-_geometry.pushUv(glm::vec2(0.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 0.0f));
-_geometry.pushUv(glm::vec2(1.0f, 1.0f));
-_geometry.pushUv(glm::vec2(0.0f, 1.0f));
-_geometry.build();
-return (true);
+	std::string path;
+	_speed = 10.0f;
+	if (_type == 0)
+		path = "./libgdl/assets/sand.tga";
+	else if (_type == 1)
+		path = "./libgdl/assets/sol.tga";
+	else
+		path = "./libgdl/assets/wood.tga";
+	if(_texture.load(path) == false)
+	{
+		std::cout << "false texture" << std::endl;
+		return (false);
+	}
+	_geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
+	_geometry.pushUv(glm::vec2(0.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 1.0f));
+	_geometry.pushUv(glm::vec2(0.0f, 1.0f));
+	_geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
+	_geometry.pushUv(glm::vec2(0.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 1.0f));
+	_geometry.pushUv(glm::vec2(0.0f, 1.0f));
+	_geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
+	_geometry.pushUv(glm::vec2(0.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 1.0f));
+	_geometry.pushUv(glm::vec2(0.0f, 1.0f));
+	_geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
+	_geometry.pushUv(glm::vec2(0.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 1.0f));
+	_geometry.pushUv(glm::vec2(0.0f, 1.0f));
+	_geometry.pushVertex(glm::vec3(0.5, 0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(0.5, 0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, 0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, 0.5, 0.5));
+	_geometry.pushUv(glm::vec2(0.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 1.0f));
+	_geometry.pushUv(glm::vec2(0.0f, 1.0f));
+	_geometry.pushVertex(glm::vec3(0.5, -0.5, -0.5));
+	_geometry.pushVertex(glm::vec3(0.5, -0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, -0.5, 0.5));
+	_geometry.pushVertex(glm::vec3(-0.5, -0.5, -0.5));
+	_geometry.pushUv(glm::vec2(0.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 0.0f));
+	_geometry.pushUv(glm::vec2(1.0f, 1.0f));
+	_geometry.pushUv(glm::vec2(0.0f, 1.0f));
+	_geometry.build();
+	return (true);
 }
 
 void Graphics::Cube::update(gdl::Clock const &clock, gdl::Input &input)
 {
-
-if (input.getKey(SDLK_UP))
-translate(glm::vec3(0, 0, -1) * static_cast<float>(clock.getElapsed()) * _speed);
-if (input.getKey(SDLK_DOWN))
-translate(glm::vec3(0, 0, 1) * static_cast<float>(clock.getElapsed()) * _speed);
-if (input.getKey(SDLK_LEFT))
-translate(glm::vec3(-1, 0, 0) * static_cast<float>(clock.getElapsed()) * _speed);
-if (input.getKey(SDLK_RIGHT))
-translate(glm::vec3(1, 0, 0) * static_cast<float>(clock.getElapsed()) * _speed);
+	if (input.getKey(SDLK_UP))
+		translate(glm::vec3(0, 0, -1) * static_cast<float>(clock.getElapsed()) * _speed);
+	if (input.getKey(SDLK_DOWN))
+		translate(glm::vec3(0, 0, 1) * static_cast<float>(clock.getElapsed()) * _speed);
+	if (input.getKey(SDLK_LEFT))
+		translate(glm::vec3(-1, 0, 0) * static_cast<float>(clock.getElapsed()) * _speed);
+	if (input.getKey(SDLK_RIGHT))
+		translate(glm::vec3(1, 0, 0) * static_cast<float>(clock.getElapsed()) * _speed);
 }
 
 void Graphics::Cube::draw(gdl::AShader &shader, gdl::Clock const &clock)
 {
-(void)clock;
-// On bind la texture pour dire que l'on veux l'utiliser
-_texture.bind();
-// Et on dessine notre cube
-_geometry.draw(shader, getTransformation(), GL_QUADS);
+	(void)clock;
+	_texture.bind();
+	_geometry.draw(shader, getTransformation(), GL_QUADS);
 }
