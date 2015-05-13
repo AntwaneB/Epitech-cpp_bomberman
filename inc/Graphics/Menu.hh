3732750106713a426478971fd30106cb08b768e4
thomas moritz <thomas.moritz@epitech.eu>
2015-05-13 16:07:17 +0200

    menu graph

--- inc/Graphics/Menu.hh
+++ inc/Graphics/Menu.hh
@@ -8,19 +8,29 @@
 #ifndef MENU_HH
 #define	MENU_HH
 
+#include "Observer.hpp"
+#include "SdlContext.hh"
+#include "Game.hh"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 namespace Graphics
 {
-
 	class Menu
 	{
 	public:
 		Menu();
 		virtual ~Menu();
+		void	update(int, int);
 
 	private:
+		sf::RenderWindow	_window;
+		sf::Text 		_txt;
+		sf::Texture		_tBackground;
+		sf::Texture		_tArrow;
+		sf::Sprite		_background;
+		sf::Sprite		_arrow;		
 
 	};
-
-}
-
+};
 #endif	/* MENU_HH */
