3a7b5e65b82d80a9fdde2de0dcc7eb3e56eaa067
Antoine Buchser <antoine.buchser@epitech.eu>
2015-05-17 11:32:42 +0200

    indentation

--- inc/IA.hpp
+++ inc/IA.hpp
@@ -74,48 +74,48 @@ IA<T>::~IA()
 template<Difficulty T>
 bool    IA<T>::scanMapForEnemy(Character::Action & action)
 {
-        std::vector<int>	searchX = {0, 1, 0, -1};
-        std::vector<int>	searchY = {-1, 0, 1, 0};
-        Character::Action   currentDirection;
-        int             	currentX;
-        int             	currentY;
-        int             	mapHeight = _lvl->map().height();
-        int             	mapWidth = _lvl->map().width();
-        int             	myX = _self->position().x();
-        int             	myY = _self->position().y();
-        int             	i = 0;
- 
-        if (_escapeNodes.size() == 0)
-        {
-            std::cout << "[scanMapForEnemy()] : No possible path !" << std::endl;
-            action = Character::WAIT;
-            return (false);
-        }
-        else
-        {
-	        currentX = _escapeNodes.front().x();
-	        currentY = _escapeNodes.front().y();
-	        _escapeNodes.pop_front();
-	        currentDirection = (_strategyMap[currentX][currentY]).direction();
-	        while (i < 4)
-	        {
-	            if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight && _strategyMap[myX + searchX[i]][myY + searchY[i]].direction() == Character::WAIT)
-                {
-	                if (_strategyMap[myX + searchX[i]][myY + searchY[i]].free() == true && _strategyMap[myX + searchX[i]][myY + searchY[i]].explosion() == false)
-	                {
-                        std::cout << "IA : nearest reacheable explosion-free node found !" << std::endl;
-                        return (currentDirection);
-	                }
-	                else
-                    {
-                        _strategyMap[myX + searchX[i]][myY + searchY[i]].setDirection(currentDirection);
-                        _escapeNodes.push_back(Position(myX + searchX[i], myY + searchY[i]));
-                    }
-                }
-	            i++;
-	        }
-        }
-        return (false);
+	std::vector<int>	searchX = {0, 1, 0, -1};
+	std::vector<int>	searchY = {-1, 0, 1, 0};
+	Character::Action   currentDirection;
+	int             	currentX;
+	int             	currentY;
+	int             	mapHeight = _lvl->map().height();
+	int             	mapWidth = _lvl->map().width();
+	int             	myX = _self->position().x();
+	int             	myY = _self->position().y();
+	int             	i = 0;
+
+	if (_escapeNodes.size() == 0)
+	{
+		 std::cout << "[scanMapForEnemy()] : No possible path !" << std::endl;
+		 action = Character::WAIT;
+		 return (false);
+	}
+	else
+	{
+		currentX = _escapeNodes.front().x();
+		currentY = _escapeNodes.front().y();
+		_escapeNodes.pop_front();
+		currentDirection = (_strategyMap[currentX][currentY]).direction();
+		while (i < 4)
+		{
+			 if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight && _strategyMap[myX + searchX[i]][myY + searchY[i]].direction() == Character::WAIT)
+			  {
+				  if (_strategyMap[myX + searchX[i]][myY + searchY[i]].free() == true && _strategyMap[myX + searchX[i]][myY + searchY[i]].explosion() == false)
+				  {
+						 std::cout << "IA : nearest reacheable explosion-free node found !" << std::endl;
+						 return (currentDirection);
+				  }
+				  else
+					{
+						 _strategyMap[myX + searchX[i]][myY + searchY[i]].setDirection(currentDirection);
+						 _escapeNodes.push_back(Position(myX + searchX[i], myY + searchY[i]));
+					}
+			  }
+			 i++;
+		}
+	}
+	return (false);
 }
 
 template<Difficulty T>
@@ -167,120 +167,120 @@ void IA<T>::scanMap()
 template<>
 inline Character::Action IA<EASY>::Move()
 {
-        std::vector<Character::Action>  searchActions = { Character::MOVE_UP, Character::MOVE_RIGHT, Character::MOVE_DOWN, Character::MOVE_LEFT};
-        std::vector<Character::Action>  possibleDirections;
-        std::vector<int>            	searchX = {0, 1, 0, -1};
-        std::vector<int>           		searchY = {-1, 0, 1, 0};
-        int             				mapHeight = _lvl->map().height();
-        int             				mapWidth = _lvl->map().width();
-        int             				myX = _self->position().x();
-        int             				myY = _self->position().y();
-        int             				i = 0;
- 
-        std::cout << "running MOVE(EASY)" << std::endl;
-        while (i < 4)
-        {
-                if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight)
-                {
-                    if (_strategyMap[myX + searchX[i]][myY + searchY[i]].free())
-                            possibleDirections.push_back(searchActions[i]);
-                }
-                i++;
-        }
-        std::cout << "[MOVE(EASY)] found : " << possibleDirections.size() << " possible directions" << std::endl;
-        if (possibleDirections.size() != 0)
-                return (possibleDirections[rand() % possibleDirections.size()]);
-        else
-                return (Character::WAIT);
+	std::vector<Character::Action>  searchActions = { Character::MOVE_UP, Character::MOVE_RIGHT, Character::MOVE_DOWN, Character::MOVE_LEFT};
+	std::vector<Character::Action>  possibleDirections;
+	std::vector<int>            	searchX = {0, 1, 0, -1};
+	std::vector<int>           		searchY = {-1, 0, 1, 0};
+	int             				mapHeight = _lvl->map().height();
+	int             				mapWidth = _lvl->map().width();
+	int             				myX = _self->position().x();
+	int             				myY = _self->position().y();
+	int             				i = 0;
+
+	std::cout << "running MOVE(EASY)" << std::endl;
+	while (i < 4)
+	{
+		if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight)
+		{
+			if (_strategyMap[myX + searchX[i]][myY + searchY[i]].free())
+				possibleDirections.push_back(searchActions[i]);
+		}
+		i++;
+	}
+	std::cout << "[MOVE(EASY)] found : " << possibleDirections.size() << " possible directions" << std::endl;
+	if (possibleDirections.size() != 0)
+		return (possibleDirections[rand() % possibleDirections.size()]);
+	else
+		return (Character::WAIT);
 }
- 
+
 template<>
 inline Character::Action IA<MEDIUM>::Move()
 {
-    std::vector<Character::Action>  searchActions = { Character::MOVE_UP, Character::MOVE_RIGHT, Character::MOVE_DOWN, Character::MOVE_LEFT};
-    std::vector<int>   				searchX = {0, 1, 0, -1};
-    std::vector<int>   				searchY = {-1, 0, 1, 0};
-    Character::Action   			currentBestAction = Character::WAIT;
-    int             				currentBestDirectionHistory = 5000;
-    int             				mapHeight = _lvl->map().height();
-    int             				mapWidth = _lvl->map().width();
-    int             				myX = _self->position().x();
-    int             				myY = _self->position().y();
-    int             				i = 0;
+	std::vector<Character::Action>  searchActions = { Character::MOVE_UP, Character::MOVE_RIGHT, Character::MOVE_DOWN, Character::MOVE_LEFT};
+	std::vector<int>   				searchX = {0, 1, 0, -1};
+	std::vector<int>   				searchY = {-1, 0, 1, 0};
+	Character::Action   			currentBestAction = Character::WAIT;
+	int             				currentBestDirectionHistory = 5000;
+	int             				mapHeight = _lvl->map().height();
+	int             				mapWidth = _lvl->map().width();
+	int             				myX = _self->position().x();
+	int             				myY = _self->position().y();
+	int             				i = 0;
 
-    std::cout << "running MOVE(MEDIUM)" << std::endl;
-    while (i < 4)
-    {
-        if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight)
-        {
-            if (_history[myX + searchX[i]][myY + searchY[i]] < currentBestDirectionHistory && _strategyMap[myX + searchX[i]][myY + searchY[i]].direction() == Character::WAIT)
-            {
-                currentBestDirectionHistory = _history[myX + searchX[i]][myY + searchY[i]];
-                currentBestAction = searchActions[i];
-            }
-        }
-        i++;
-    }
-    return (currentBestAction);
+	std::cout << "running MOVE(MEDIUM)" << std::endl;
+	while (i < 4)
+	{
+		if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight)
+		{
+			if (_history[myX + searchX[i]][myY + searchY[i]] < currentBestDirectionHistory && _strategyMap[myX + searchX[i]][myY + searchY[i]].direction() == Character::WAIT)
+			{
+				currentBestDirectionHistory = _history[myX + searchX[i]][myY + searchY[i]];
+				currentBestAction = searchActions[i];
+			}
+		}
+		i++;
+	}
+	return (currentBestAction);
 }
 
 template<>
 inline Character::Action IA<HARD>::Move()
 {
-        std::vector<Character::Action>  searchActions = { Character::MOVE_UP, Character::MOVE_RIGHT, Character::MOVE_DOWN, Character::MOVE_LEFT};
-        std::vector<int>                                searchX = {0, 1, 0, -1};
-        std::vector<int>                                searchY = {-1, 0, 1, 0};
-        Character::Action                               finalAction = Character::WAIT;
-        bool    enemyDirectionFound = false;
-        int             myX = _self->position().x();
-        int             myY = _self->position().y();
-        int             mapHeight = _lvl->map().height();
-        int             mapWidth = _lvl->map().width();
-        int             i = 0;
- 
-        std::cout << "running MOVE(HARD)" << std::endl;
-        _strategyMap[myX][myY].setDirection(Character::MOVE_UP);
-        while (i < 4)
-        {
-            if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight && _strategyMap[myX + searchX[i]][myY + searchY[i]].free() == true)
-            {
-                _strategyMap[myX + searchX[i]][myY + searchY[i]].setDirection(searchActions[i]);
-                _escapeNodes.push_back(Position(myX + searchX[i], myY + searchY[i]));
-            }
-        	i++;
-        }
-        while (enemyDirectionFound == false && _lvl->characters().size() > 1)
-        {
-            enemyDirectionFound = scanMapForEnemy(finalAction);
-        }
-        return finalAction;
+	std::vector<Character::Action>  searchActions = { Character::MOVE_UP, Character::MOVE_RIGHT, Character::MOVE_DOWN, Character::MOVE_LEFT};
+	std::vector<int>                                searchX = {0, 1, 0, -1};
+	std::vector<int>                                searchY = {-1, 0, 1, 0};
+	Character::Action                               finalAction = Character::WAIT;
+	bool    enemyDirectionFound = false;
+	int             myX = _self->position().x();
+	int             myY = _self->position().y();
+	int             mapHeight = _lvl->map().height();
+	int             mapWidth = _lvl->map().width();
+	int             i = 0;
+
+	std::cout << "running MOVE(HARD)" << std::endl;
+	_strategyMap[myX][myY].setDirection(Character::MOVE_UP);
+	while (i < 4)
+	{
+		if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight && _strategyMap[myX + searchX[i]][myY + searchY[i]].free() == true)
+		{
+			_strategyMap[myX + searchX[i]][myY + searchY[i]].setDirection(searchActions[i]);
+			_escapeNodes.push_back(Position(myX + searchX[i], myY + searchY[i]));
+		}
+		i++;
+	}
+	while (enemyDirectionFound == false && _lvl->characters().size() > 1)
+	{
+		 enemyDirectionFound = scanMapForEnemy(finalAction);
+	}
+	return finalAction;
 }
- 
+
 template<Difficulty T>
 inline bool IA<T>::BombOpportunity() // pose une bombe a +2 minimum de l ennemi
 {
-        std::vector<int>  	searchX = {0, 2, 0, -2};
-        std::vector<int>  	searchY = {-2, 0, 2, 0};
-        int             	myX = _self->position().x();
-        int             	myY = _self->position().y();
-        int             	mapHeight = _lvl->map().height();
-        int             	mapWidth = _lvl->map().width();
-        int             	i = 0;
- 
-        std::cout << "processing BombOpportunity()..." << std::endl;
-        while (i < 8)
-        {
-            if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight && _strategyMap[myX + searchX[i]][myY + searchY[i]].enemy() == true)
-            {
-                std::cout << "IA advise to DROP_BOMB!" << std::endl;
-                return (true);
-            }
-        	i++;
-        }
-        std::cout << "IA : useless to drom bomb here" << std::endl;
-        return (false);
+	std::vector<int>  	searchX = {0, 2, 0, -2};
+	std::vector<int>  	searchY = {-2, 0, 2, 0};
+	int             	myX = _self->position().x();
+	int             	myY = _self->position().y();
+	int             	mapHeight = _lvl->map().height();
+	int             	mapWidth = _lvl->map().width();
+	int             	i = 0;
+
+	std::cout << "processing BombOpportunity()..." << std::endl;
+	while (i < 8)
+	{
+		 if ((myX + searchX[i]) >= 0 && (myX + searchX[i]) < mapWidth && (myY + searchY[i]) >= 0 && (myY + searchY[i]) < mapHeight && _strategyMap[myX + searchX[i]][myY + searchY[i]].enemy() == true)
+		 {
+			  std::cout << "IA advise to DROP_BOMB!" << std::endl;
+			  return (true);
+		 }
+	 i++;
+	}
+	std::cout << "IA : useless to drom bomb here" << std::endl;
+	return (false);
 }
- 
+
 template<>
 inline bool IA<EASY>::BombOpportunity() // pose une bombe a +1 minimum de l ennemi
 {
