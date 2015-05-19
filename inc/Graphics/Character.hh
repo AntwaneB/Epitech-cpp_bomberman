ccc4da1f94b03f6287daee80bd61040c77b5bdb2
François Kiene <francois.kiene@epitech.eu>
2015-05-19 17:59:47 +0200

    Update map

--- inc/Graphics/Character.hh
+++ inc/Graphics/Character.hh
@@ -17,18 +17,21 @@ namespace Graphics
 	class Character : public Object
 	{
    public:
+      enum direction {UP, LEFT, DOWN, RIGHT};
       Character(Position const &, ::Character const *);
       virtual ~Character();
 
       virtual bool  initialize();
       virtual void  update(gdl::Clock const &clock, gdl::Input &input);
       virtual void  draw(gdl::AShader &shader, gdl::Clock const &clock);
+      int getAngle(direction);
 
 	private:
 		float				_speed;
 
       gdl::Texture	_texture;
       gdl::Model		_model;
+      direction _pos = UP;
 
 		const ::Character* _character;
 	};
